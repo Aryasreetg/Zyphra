b6e3de4503204471fa2725d3fe773b0c06c4828b
ARYASREE <aryasree@ARYASREEs-MacBook-Air.local>
2026-05-30 20:00:23 +0530

    proce hiked

--- app/courses/[id]/page.tsx
+++ app/courses/[id]/page.tsx
@@ -30,7 +30,7 @@ const coursesData = [
     id: "frontend-mastery",
     title: "Frontend Mastery",
     duration: "1.5 Months",
-    originalPrice: "₹12,000",
+    originalPrice: "15,000",
     offerPrice: "₹8,000",
     category: "specialisation",
     description:
